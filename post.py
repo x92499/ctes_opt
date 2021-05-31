5afc532f6e255d6d7518c0287232f60c02f395e5
x92499 <kheine@mines.edu>
2021-05-31 17:04:35 -0600

    Post.py figures

--- post.py
+++ post.py
@@ -99,6 +99,21 @@ for k in keys:
         for row in rdr:
             optimized[k].append([float(i) for i in row])
 #-------------------------------------------------------------------------------
+## Get cooling load after seasonal schedule applied
+chiller_loads = [[] for i in range(len(optimized["PX"][0]))]
+chiller_power = [[] for i in range(len(optimized["PX"][0]))]
+community["evap_cooling_rate"] = [0 for i in range(len(optimized["PX"]))]
+idx = 0
+for p in plant_loops:
+    for c in plant_loops[p]:
+        if c in ["district_cooling_load", "district_mass_flow", "total_power"]:
+            continue
+        for t in range(len(community["dtg"])):
+            chiller_loads[idx].append(plant_loops[p][c]["evap_cooling_rate"][t])
+            chiller_power[idx].append(plant_loops[p][c]["electricity_rate"][t])
+            community["evap_cooling_rate"][t] += chiller_loads[idx][-1]
+        idx += 1
+#-------------------------------------------------------------------------------
 ## Populate high-level summary data
 cool_elec = [0 for t in range(len(community["dtg"]))]
 for t in range(len(community["dtg"])):
@@ -164,4 +179,46 @@ fig.update_xaxes(zeroline=True, zerolinecolor="black")
 fig.write_html(os.path.join(figs_path, "Electric Profile.html"),
     auto_open=show_figs)
 #-------------------------------------------------------------------------------
-## Electric Load Profiles for Community
+## Themral Load Profiles for Community
+fig = go.Figure(go.Scatter(
+    x=community["dtg"], y=[i / 1000 for i in community["evap_cooling_rate"]],
+    name="Baseline", line=dict(dash="dash", color="black")))
+fig.add_trace(go.Scatter(
+    x=community["dtg"], y=optimized["cooling_thermal_rate"],
+    name="Optimal", line=dict(color="#ABD1F3")))
+fig.update_layout(title="Community Thermal Cooling Load by Timestep",
+    yaxis=dict(title="kWt", gridcolor="whitesmoke",
+        rangemode="tozero", zeroline=True, zerolinecolor="lightslategray"),
+    paper_bgcolor="white", plot_bgcolor="white")
+fig.update_xaxes(zeroline=True, zerolinecolor="black")
+fig.write_html(os.path.join(figs_path, "Cooling Load Profile.html"),
+    auto_open=show_figs)
+#-------------------------------------------------------------------------------
+## Load duration curves
+fig = sp.make_subplots(rows=2, cols=1, shared_xaxes=True,
+    subplot_titles=("Total Electricity", "Coincident Cooling Electricity"))
+ld, cld = zip(*sorted(zip(community["electricity_rate"],
+    community["cooling_electricity_rate"]), reverse=True))
+ldo, cldo = zip(*sorted(zip(optimized["electricity_rate"],
+    optimized["cooling_electricity_rate"]), reverse=True))
+fig.add_trace(go.Scatter(y=[i / 1000 for i in ld],
+    name="Baseline", line=dict(dash="dash", color="black")),
+    row=1, col=1)
+fig.add_trace(go.Scatter(y=ldo,
+    name="Optimal", line=dict(color="#ABD1F3")),
+    row=1, col=1)
+fig.add_trace(go.Scatter(y=[i / 1000 for i in cld],
+    name="Baseline Cooling", line=dict(dash="dash", color="black")),
+    row=2, col=1)
+fig.add_trace(go.Scatter(y=[i / 1000 for i in cldo],
+    name="Optimal Cooling", line=dict(color="#ABD1F3")),
+    row=2, col=1)
+fig.update_layout(title="Electricity Load Duration Curves",
+    yaxis=dict(title="MWe"), yaxis2=dict(title="MWe"),
+    paper_bgcolor="white", plot_bgcolor="white")
+fig.update_yaxes(gridcolor="whitesmoke", rangemode="tozero", zeroline=True,
+    zerolinecolor="lightslategray")
+fig.update_xaxes(rangemode="tozero", zeroline=True,
+    zerolinecolor="lightslategray")
+fig.write_html(os.path.join(figs_path, "Load Duration Curve.html"),
+    auto_open=show_figs)
